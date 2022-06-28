4f614e53481ad2fb5f431710483a004565475896
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-06-28 16:44:21 -0500

    njkuhnpiui

--- acquire.py
+++ acquire.py
@@ -1,18 +1,13 @@
 import pandas as pd
 import numpy as np
-import os
-
-from env import host, user, password
 
 
 def get_mush_data():
     '''
-    This function reads in iris data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
+    This function reads mushroom csv file and imports it in.
     '''
-    if os.path.isfile('mushrooms.csv'):
         
         # If csv file exists read in data from csv file
-        df = pd.read_csv('mushrooms.csv', index_col=0)
+    df = pd.read_csv('mushrooms.csv', index_col=0)
 
     return df 
