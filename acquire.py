cd180c35ff3059684ad0e883f4250c252e746903
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-06-28 16:51:38 -0500

    123tgfhb cgxfze mgfrew

--- acquire.py
+++ acquire.py
@@ -4,10 +4,7 @@ import numpy as np
 
 def get_mush_data():
     '''
-    This function reads mushroom csv file and imports it in.
+    # Read csv file into pandas DataFrame. 
     '''
-        
-        # If csv file exists read in data from csv file
-    df = pd.read_csv('mushrooms.csv', index_col=0)
-
-    return df 
+file = “/Users/cheese_enchilada/Desktop/Mushroom/mushrooms.csv"
+df = pd.read_csv(file)
