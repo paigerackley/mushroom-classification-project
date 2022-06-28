2f8544073cb1e78a9dcad77c4d8c31fc7e09ab49
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-06-28 16:33:19 -0500

    testing acquire, starting scratch

---
+++ acquire.py
@@ -0,0 +1,11 @@
+def get_telco_data():
+    '''
+    This function reads in iris data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('mushrooms.csv'):
+        
+        # If csv file exists read in data from csv file
+        df = pd.read_csv('mushrooms.csv', index_col=0)
+
+    return df 
