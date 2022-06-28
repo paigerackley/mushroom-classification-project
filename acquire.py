83c84e1a1e370893ea7c358b9b1c7d60bbf1692c
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-06-28 17:02:03 -0500

    idk

--- acquire.py
+++ acquire.py
@@ -2,9 +2,3 @@ import pandas as pd
 import numpy as np
 
 
-def get_mush_data():
-    '''
-    # Read csv file into pandas DataFrame. 
-    '''
-file = “/Users/cheese_enchilada/Desktop/Mushroom/mushrooms.csv"
-df = pd.read_csv(file)
